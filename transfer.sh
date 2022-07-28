3131726457e47297e29d5705b89f754baf67fc1a
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-07-28 22:36:02 +0200

    add fix shell chech

--- transfer.sh
+++ transfer.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 #set -x
 # This script to upload files from terminal to https://transfer.sh 
-currentVersion="1.4.0"
+currentVersion="1.23.0"
 
 singleUpload()
 {
-  for file in $@
-  do
-  echo "Uploading $file"
-  response=$(curl --progress-bar --upload-file "$1" "https://transfer.sh/$file") || { echo "Failure!"; return 1;}
+  for i in "$@"; do
+    file=$i
+    echo Uploading "$i"
+    response=$(curl --progress-bar --upload-file "$1" "https://transfer.sh/$file") || { echo  Failure!; return 1;}
+    echo Transfer File URL: "$response" 
   done
 }
 
-printUploadResponse()
-{
-fileID=$(echo "$response" | cut -d "/" -f 4)
-  cat <<EOF
-Transfer File URL: $response
-EOF
-}
+#printUploadResponse()
+#{
+#fileID=$(echo "$response" | cut -d "/" -f 4)
+#  cat <<EOF
+#Transfer File URL: $response
+#EOF
+#}
 
 singleDowload() {
-  curl https://transfer.sh/$2/$3 -o $3
+  echo Downloading "$4"
+  curl --progress-bar https://transfer.sh/"$2"/"$3" -o "$3"
+  echo "Success!"
 }
 
 Help()
--- transfer_clear.sh
+++ transfer_clear.sh
@@ -1,3 +1,51 @@
 #!/bin/bash
+#set -x
+# This script to upload files from terminal to https://transfer.sh 
+currentVersion="1.23.0"
 
-echo "Done"
+singleUpload()
+{
+  for i in "$@"; do
+    file=$i
+    echo "\n Uploading $i"
+    response=$(curl --progress-bar --upload-file "$1" "https://transfer.sh/$file") || { echo -e "\033[31m Failure!\033[37m"; return 1;}
+    echo -e "\033[32m Transfer File URL: ""$response"" \n" 
+  done
+}
+
+printUploadResponse()
+{
+fileID=$(echo "$response" | cut -d "/" -f 4)
+  cat <<EOF
+Transfer File URL: $response
+EOF
+}
+
+singleDowload() {
+  curl https://transfer.sh/$2/$3 -o $3
+}
+
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-g|h|v|V]"
+   echo "options:"
+   echo "g     Print the GPL license notification."
+   echo "h     Print this Help."
+   echo "v     Verbose mode."
+   echo "V     Print software version and exit."
+   echo
+}
+
+if [[ $1 == "-v" ]]; then
+  echo "$currentVersion" && exit
+elif [[ $1 == "-h" ]]; then
+  Help && exit
+elif [[ $1 == "-d"* ]]; then
+  singleDowload "$@" && exit
+fi
+
+singleUpload "$@" || exit 1
+printUploadResponse
